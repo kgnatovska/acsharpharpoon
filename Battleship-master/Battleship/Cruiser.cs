b5a00a4878bc5312d3975e488ca355878680b125
kgnatovska <k.gnatovska@gmail.com>
2015-11-15 00:29:28 +0200

    battleship commit

---
+++ Battleship-master/Battleship/Cruiser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class Cruiser : Ship
+    {
+        public Cruiser(uint x, uint y) : this(x, y, Directions.Horizontal)
+        {
+        }
+
+        public Cruiser(uint x, uint y, Directions direction) : base(x, y, 2, direction)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            Cruiser cruiser = obj as Cruiser;
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+            return X == cruiser.X && Y == cruiser.Y && Direction == cruiser.Direction;
+        }
+
+        public bool Equals(Cruiser cruiser)
+        {
+            if (cruiser == null)
+                return false;
+            return X == cruiser.X && Y == cruiser.Y && Direction == cruiser.Direction;
+        }
+
+        public static bool operator ==(Cruiser a, Cruiser b)
+        {
+            if (object.ReferenceEquals(a, b))
+                return true;
+            return a.X == b.X && a.Y == b.Y && a.Direction == b.Direction;
+        }
+
+        public static bool operator !=(Cruiser a, Cruiser b)
+        {
+            return !(a == b);
+        }
+    }
+}
---
+++ Battleship-master/Battleship/PatrolBoat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class PatrolBoat : Ship
+    {
+        public PatrolBoat(uint x, uint y) : this(x, y, Directions.Horizontal)
+        {
+        }
+
+        public PatrolBoat(uint x, uint y, Directions direction) : base(x, y, 1, direction)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            PatrolBoat boat = obj as PatrolBoat;
+            if (obj == null || obj.GetType() != this.GetType())
+                return false;
+            return X == boat.X && Y == boat.Y;
+        }
+
+        public bool Equals(PatrolBoat boat)
+        {
+            if (boat == null)
+                return false;
+            return X == boat.X && Y == boat.Y;
+        }
+
+        public static bool operator ==(PatrolBoat a, PatrolBoat b)
+        {
+            if (object.ReferenceEquals(a, b))
+                return true;
+            return a.X == b.X && a.Y == b.Y;
+        }
+
+        public static bool operator !=(PatrolBoat a, PatrolBoat b)
+        {
+            return !(a == b);
+        }
+    }
+}
